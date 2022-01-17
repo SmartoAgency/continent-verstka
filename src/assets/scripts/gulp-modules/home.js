c53ef3f83c290898486ad5984c42fa05c25ca85f
Aleksandra Pedan <sashka28465@gmail.com>
2022-01-17 15:51:59 +0200

    single projects

--- src/assets/scripts/gulp-modules/home.js
+++ src/assets/scripts/gulp-modules/home.js
@@ -274,37 +274,37 @@ $('.video')
     }
   });
 
-// btn social start
-const socialBtn = document.querySelectorAll('.js-social');
-socialBtn.forEach(el => {
-  el.addEventListener('click', () => {
-    el.classList.toggle('social-link__grow');
-  });
-});
+// // btn social start
+// const socialBtn = document.querySelectorAll('.js-social');
+// socialBtn.forEach(el => {
+//   el.addEventListener('click', () => {
+//     el.classList.toggle('social-link__grow');
+//   });
+// });
 
-function handleVisibilityOnScroll(elems = [], direction = 'up') {
-  elems.forEach(elem => {
-    switch (direction) {
-      case 'down':
-        elem[0].classList.add(elem[1]);
-        break;
-      default:
-        elem[0].classList.remove(elem[1]);
-        break;
-    }
-  });
-}
-const socialSelect = document.querySelectorAll('.social-select');
-locoScroll.on('scroll', position => {
-  if (position.scroll.y > 100) {
-    handleVisibilityOnScroll([[socialSelect[0], 'visible']], 'down');
-    handleVisibilityOnScroll([[socialSelect[1], 'visible']], 'down');
-  } else {
-    handleVisibilityOnScroll([[socialSelect[1], 'visible']]);
-    handleVisibilityOnScroll([[socialSelect[0], 'visible']]);
-  }
-});
-// btn social end
+// function handleVisibilityOnScroll(elems = [], direction = 'up') {
+//   elems.forEach(elem => {
+//     switch (direction) {
+//       case 'down':
+//         elem[0].classList.add(elem[1]);
+//         break;
+//       default:
+//         elem[0].classList.remove(elem[1]);
+//         break;
+//     }
+//   });
+// }
+// const socialSelect = document.querySelectorAll('.social-select');
+// locoScroll.on('scroll', position => {
+//   if (position.scroll.y > 100) {
+//     handleVisibilityOnScroll([[socialSelect[0], 'visible']], 'down');
+//     handleVisibilityOnScroll([[socialSelect[1], 'visible']], 'down');
+//   } else {
+//     handleVisibilityOnScroll([[socialSelect[1], 'visible']]);
+//     handleVisibilityOnScroll([[socialSelect[0], 'visible']]);
+//   }
+// });
+// // btn social end
 
 // const socialSelect = document.querySelectorAll('.social-select');
 // window.onscroll = function() {
--- src/assets/scripts/gulp-modules/project-realized.js
+++ src/assets/scripts/gulp-modules/project-realized.js
@@ -8,6 +8,35 @@ var swiper = new Swiper('.realized-project', {
     clickable: true,
   },
   autoplay: {
-    delay: 5000,
+    delay: 4000,
+  },
+});
+
+function addZero(num) {
+  return num > 9 ? num : `0${num}`;
+}
+
+var swiper2 = new Swiper('.description-swiper', {
+  spaceBetween: 30,
+  slidesPerView: 1.25,
+  // freeMode: true,
+  watchSlidesProgress: true,
+  // loop: true,
+  // autoplay: {
+  //   delay: 1000,
+  // },
+  navigation: {
+    nextEl: '.swiper-button-next1',
+    prevEl: '.swiper-button-prev1',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar1',
+  },
+  pagination: {
+    el: '.swiper-pagination1',
+    type: 'fraction',
+    // touchStartPreventDefault: false,
+    formatFractionCurrent: addZero,
+    formatFractionTotal: addZero,
   },
 });
