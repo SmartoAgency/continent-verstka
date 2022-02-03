8b9f316e1beb0325dab1cc42a6a9140704466b5b
Aleksandra Pedan <sashka28465@gmail.com>
2022-02-03 16:52:10 +0200

    mobile start

--- src/assets/scripts/gulp-modules/header-menu.js
+++ src/assets/scripts/gulp-modules/header-menu.js
@@ -24,3 +24,53 @@ menuClose.addEventListener('click', () => {
   header.classList.remove('menu-header');
   document.querySelector('body').style.overflow = 'auto';
 });
+
+// Mobile phone menu start
+const btnCallMobile = document.querySelectorAll('.js-mobile-call');
+const btnCloseMobile = document.querySelector('.js-mobile-close');
+const formMobile = document.querySelector('.form-header-call');
+// const formCallMobile = document.querySelector('.js-mobile-form');
+// formCallMobile.addEventListener('click', () => {
+//   formCall.classList.add('sideform-active');
+//   document.querySelector('body').style.overflow = 'hidden';
+// });
+console.log('aaaaa', btnCallMobile);
+btnCallMobile.forEach(el =>
+  el.addEventListener('click', () => {
+    formMobile.classList.toggle('sideform-active');
+    document.querySelector('body').style.overflow = 'hidden';
+  }),
+);
+
+btnCloseMobile.addEventListener('click', () => {
+  formMobile.classList.remove('sideform-active');
+  document.querySelector('body').style.overflow = 'auto';
+  formGratitude.classList.remove('sideform-active');
+  document.querySelector('body').style.overflow = 'auto';
+});
+formMobile.addEventListener('click', onBackdropClick);
+function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    formMobile.classList.remove('sideform-active');
+    document.querySelector('body').style.overflow = 'auto';
+  }
+}
+// Mobile phone menu end
+
+// bottom-header start
+const btnBottomHeader = document.querySelectorAll('.js-mobile-bottom');
+const headerList = document.querySelector('.js-header-list');
+
+btnBottomHeader.forEach(el =>
+  el.addEventListener('click', () => {
+    headerList.classList.toggle('open-header-menu');
+    el.classList.toggle('open-mobile-list');
+    if (headerList.classList.contains('open-header-menu')) {
+      document.querySelector('body').style.overflow = 'hidden';
+    } else {
+      document.querySelector('body').style.overflow = 'initial';
+    }
+  }),
+);
+window.locoScroll.update();
+// bottom-header end
