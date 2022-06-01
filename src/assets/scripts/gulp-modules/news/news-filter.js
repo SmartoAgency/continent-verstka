0b9ffbd7c522934621322f2b2b750392de3d74da
unknown <mr.andrey@bigmir.net>
2022-06-01 17:43:42 +0300

    add markers, some styles, header bottom corrections, news filter connect to actual data

--- src/assets/scripts/gulp-modules/news/news-filter.js
+++ src/assets/scripts/gulp-modules/news/news-filter.js
@@ -1,6 +1,48 @@
+// /* eslint-disable no-undef */
+// async function newsFilter() {
+//   let request = await fetch('./static/test-news.json');
+//   request = await request.json();
+
+//   const filterConfig = new FilterConfig();
+//   filterConfig.importContent(request);
+//   const filteredList = new FilteredList({
+//     customTemplate: (data) => {
+//       const {
+//         date, title, img, type, complex,
+//       } = data;
+//       return `
+//         <li class="news-item"> <a class="news-link" href="single-news.html">
+//           <div class="news-img"> <img src="${img}" alt=""></div>
+//           <div class="news-text">
+//             <h5 class="date">${date}</h5>
+//             <h4 class="title">${title}</h4>
+//           </div>
+//           <ul class="markers">
+//             <li ${type === 'promotion' ? '' : 'style="display: none"'} class="markers-item">${type}</li>
+//             <li class="markers-item">${complex}</li>
+//           </ul></a>
+//         </li>
+//       `;
+//     },
+//   });
+//   filteredList.import(filterConfig.filter());
+//   window.addEventListener('filtering', () => {
+//     filteredList.import(filterConfig.filter());
+//   });
+// }
+
+// newsFilter();
+
+
 /* eslint-disable no-undef */
 async function newsFilter() {
-  let request = await fetch('./static/test-news.json');
+  const url = window.location.href.match(/localhost/) ? './static/test-news.json' : '/wp-admin/admin-ajax.php';
+  const sendData = new FormData();
+  sendData.append('action', 'news');
+  let request = await fetch(url, {
+    body: sendData,
+    method: 'POST',
+  });
   request = await request.json();
 
   const filterConfig = new FilterConfig();
@@ -10,19 +52,7 @@ async function newsFilter() {
       const {
         date, title, img, type, complex,
       } = data;
-      return `
-        <li class="news-item"> <a class="news-link" href="single-news.html">
-          <div class="news-img"> <img src="${img}" alt=""></div>
-          <div class="news-text">
-            <h5 class="date">${date}</h5>
-            <h4 class="title">${title}</h4>
-          </div>
-          <ul class="markers"> 
-            <li ${type === 'promotion' ? '' : 'style="display: none"'} class="markers-item">${type}</li>
-            <li class="markers-item">${complex}</li>
-          </ul></a>
-        </li>
-      `;
+      return data.layout;
     },
   });
   filteredList.import(filterConfig.filter());
--- src/assets/scripts/gulp-modules/no-scroll.js
+++ src/assets/scripts/gulp-modules/no-scroll.js
@@ -1,36 +1,41 @@
-document.addEventListener('DOMContentLoaded', () => {
-  window.locoScroll.destroy();
-});
-let prevScrollPosition = 0;
-const headerBottom = document.querySelector('.js-header-bottom');
-/**
- * 1 - down
- * 0 - up
- */
-const onscrollHeaderActions = [
-  () => {
-    document.getElementById('header').classList.remove('not-on-top');
-    headerBottom.classList.add('on-top');
-  },
-  () => {
-    document.getElementById('header').classList.add('not-on-top');
-    headerBottom.classList.remove('on-top');
-  },
-];
-window.onscroll = function (e) {
-  const currentY = window.pageYOffset;
 
-  const direction = prevScrollPosition < currentY ? 1 : 0;
 
-  onscrollHeaderActions[direction]();
-  prevScrollPosition = currentY;
-  // scrollFunction();
-};
+function noScroll() {
+  document.addEventListener('DOMContentLoaded', () => {
+    window.locoScroll.destroy();
+  });
+  let prevScrollPosition = 0;
+  const headerBottom = document.querySelector('.js-header-bottom');
+  /**
+   * 1 - down
+   * 0 - up
+   */
+  const onscrollHeaderActions = [
+    () => {
+      document.getElementById('header').classList.remove('not-on-top');
+      headerBottom.classList.remove('active-header-menu');
+    },
+    () => {
+      document.getElementById('header').classList.add('not-on-top');
+      headerBottom.classList.add('active-header-menu');
+    },
+  ];
+  window.onscroll = function (e) {
+    const currentY = window.pageYOffset;
 
-// function scrollFunction() {
-//   // if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-//   //   document.getElementById('header').classList.add('not-on-top');
-//   // } else {
-//   //   document.getElementById('header').classList.remove('not-on-top');
-//   // }
-// }
+    const direction = prevScrollPosition < currentY ? 1 : 0;
+
+    onscrollHeaderActions[direction]();
+    prevScrollPosition = currentY;
+    // scrollFunction();
+  };
+
+  // function scrollFunction() {
+  //   // if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+  //   //   document.getElementById('header').classList.add('not-on-top');
+  //   // } else {
+  //   //   document.getElementById('header').classList.remove('not-on-top');
+  //   // }
+  // }
+}
+noScroll();
