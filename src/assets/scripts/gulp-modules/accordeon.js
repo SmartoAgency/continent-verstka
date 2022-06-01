8223dc063a6111f392b7fe75c35bf6c2144e551c
unknown <mr.andrey@bigmir.net>
2022-06-01 20:25:26 +0300

    accordeon fix doubling handlers, sign up form succes message hide

--- src/assets/scripts/gulp-modules/accordeon.js
+++ src/assets/scripts/gulp-modules/accordeon.js
@@ -1,33 +1,40 @@
 $(() => {
-  // Cache selectors
-  const $accordion = $('.js-accordion');
-  const $allPanels = $(' .accordion-panel').hide();
-  const $allItems = $('.accordion-item');
+  if (window.accordeons) return;
+  function accordeons() {
+    // Cache selectors
+    const $accordion = $('.js-accordion');
+    const $allPanels = $(' .accordion-panel').hide();
+    const $allItems = $('.accordion-item');
 
-  // Event listeners
-  $accordion.on('click', '.accordion-toggle', function () {
-    if (
-      $(this)
-        .next()
-        .is(':visible')
-    ) {
-      $(this)
-        .parent()
-        .removeClass('is-open');
-      $(this)
-        .next()
-        .slideUp();
-    } else {
-      $(this)
-        .next()
-        .slideDown()
-        .closest('.accordion-item')
-        .addClass('is-open');
-    }
-    setTimeout(() => {
-      window.locoScroll.update();
-    }, 700);
-    return false;
-  });
-  // openAccordeonOnAnchorLink();
+    // Event listeners
+    $accordion.on('click', '.accordion-toggle', function () {
+      console.log('CLICK');
+      if (
+        $(this)
+          .next()
+          .is(':visible')
+      ) {
+        $(this)
+          .parent()
+          .removeClass('is-open');
+        $(this)
+          .next()
+          .slideUp();
+      } else {
+        $(this)
+          .next()
+          .slideDown()
+          .closest('.accordion-item')
+          .addClass('is-open');
+      }
+      setTimeout(() => {
+        window.locoScroll.update();
+      }, 700);
+      return false;
+    });
+    // openAccordeonOnAnchorLink();
+    return true;
+  }
+
+  window.accordeons = accordeons();
 });
